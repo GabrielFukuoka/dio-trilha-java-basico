a2f9342176dedbfb3221dc7843e15d22c8b98a86
Gabbriel33 <72907575+Gabbriel33@users.noreply.github.com>
2024-06-28 02:10:32 -0300

    Operadores em Java

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,3 +1,19 @@
 public class Operadores {
-    
+    public static void main(String[] args) {
+        boolean condicao1 = true;
+        boolean condicao2 = true;
+
+        if(condicao1&&condicao2){
+            System.out.println("As duas condições são verdadeiras.");
+
+        }
+        if(condicao1 || condicao2){
+            System.out.print("Uma das condiçoes é verdadeira!");
+        }
+        else
+            System.out.println("Não entrou no if");
+
+        
+
+    }
 }
