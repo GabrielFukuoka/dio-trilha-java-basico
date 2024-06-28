c9242463da1a33a9f637dd04992eca9e62c2bb00
Gabbriel33 <72907575+Gabbriel33@users.noreply.github.com>
2024-06-27 23:58:46 -0300

    Tipos de variaveis em Java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, !");
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        
+
+        int numero = 1;
+        numero = 2;
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(VALOR_DE_PI);
+         
+
     }
 }
